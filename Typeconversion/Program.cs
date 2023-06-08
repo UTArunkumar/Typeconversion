ff734f0728d483182aa9d522f015de4706c2e963
UTArunkumar <132251495+UTArunkumar@users.noreply.github.com>
2023-06-08 12:55:06 +0530

    Add parse

--- Typeconversion/Program.cs
+++ Typeconversion/Program.cs
@@ -8,31 +8,31 @@ namespace Tpyeconversion
         {
             //Integer to char
             int c = 123;
-            String str = Convert.ToString(c);
-            Console.WriteLine("Integer to char: "+str+ "\n");
+            string str = Convert.ToString(c);
+            Console.WriteLine("Integer to char: " + str + "\n");
 
             //Char to Integer
             string strg = "1234";
             int val = Convert.ToInt32(strg);
-            Console.WriteLine("Char to Integer: "+val+ "\n");
+            Console.WriteLine("Char to Integer: " + val + "\n");
 
             //Integer to Float
             int f = 12;
             float vale = Convert.ToSingle(f);
-            Console.WriteLine("Integer to Float: "+vale+ "\n");
+            Console.WriteLine("Integer to Float: " + vale + "\n");
 
             //Int to Double
             int value = 54;
-            Double d = Convert.ToDouble(value);
-            Console.WriteLine("Int to Double: "+d+ "\n");
+            double d = Convert.ToDouble(value);
+            Console.WriteLine("Int to Double: " + d + "\n");
 
             //Float to Double
             float fd = 1.5f;
-            Double dob = Convert.ToDouble(fd);
+            double dob = Convert.ToDouble(fd);
             Console.WriteLine("Float to Double: " + dob + "\n");
 
             //String to boolean
-            string convert  = "true";
+            string convert = "true";
             bool boolvalue = Convert.ToBoolean(convert);
             Console.WriteLine("String to boolean: " + boolvalue + "\n");
 
@@ -44,22 +44,26 @@ namespace Tpyeconversion
             //Converts a type to a byte
             string stri = "10";
             byte bytevalue = Convert.ToByte(stri);
-            Console.WriteLine("string to byte: "+ bytevalue +"\n");
+            Console.WriteLine("string to byte: " + bytevalue + "\n");
 
             //Converts type to a datatime
             DateTime dt2 = DateTime.Now;
-            Console.WriteLine("update datatime: "+dt2 +"\n");
+            Console.WriteLine("update datatime: " + dt2 + "\n");
 
             //Converts type to a decimal
             string stringVal = "2,345.26";
             decimal.Parse(stringVal);
-            Console.WriteLine("convert to decimal:"+ stringVal + "\n");
+            Console.WriteLine("convert to decimal:" + stringVal + "\n");
 
 
             // float to double
             float floatNum = 3.14f;
             double doubleNum = floatNum;
-            Console.WriteLine("convert to double:" + doubleNum);
+            Console.WriteLine("convert to double:" + doubleNum + "\n");
+
+            string a = "1234";
+            int b1 = int.Parse(a);
+            Console.WriteLine("string to int: " + b1);
         }
     }
 }
