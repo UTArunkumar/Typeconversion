6e9429cdd448a14231cd41fdada09bd91a826744
UTArunkumar <132251495+UTArunkumar@users.noreply.github.com>
2023-06-08 19:22:39 +0530

    Add a Method

--- Typeconversion/Program.cs
+++ Typeconversion/Program.cs
@@ -6,64 +6,134 @@ namespace Tpyeconversion
     {
         static void Main(string[] args)
         {
-            //Integer to char
+
+            //Conversion using Convert class
+            Console.WriteLine("Convert method: "+"\n");
             int c = 123;
             string str = Convert.ToString(c);
-            Console.WriteLine("Integer to char: " + str + "\n");
+            Console.WriteLine("Integer to char: " + str );
 
-            //Char to Integer
             string strg = "1234";
             int val = Convert.ToInt32(strg);
-            Console.WriteLine("Char to Integer: " + val + "\n");
+            Console.WriteLine("Char to Integer: " + val );
 
-            //Integer to Float
             int f = 12;
             float vale = Convert.ToSingle(f);
-            Console.WriteLine("Integer to Float: " + vale + "\n");
+            Console.WriteLine("Integer to Float: " + vale );
 
-            //Int to Double
             int value = 54;
             double d = Convert.ToDouble(value);
-            Console.WriteLine("Int to Double: " + d + "\n");
+            Console.WriteLine("Int to Double: " + d  );
 
-            //Float to Double
             float fd = 1.5f;
             double dob = Convert.ToDouble(fd);
-            Console.WriteLine("Float to Double: " + dob + "\n");
+            Console.WriteLine("Float to Double: " + dob );
 
-            //String to boolean
             string convert = "true";
             bool boolvalue = Convert.ToBoolean(convert);
-            Console.WriteLine("String to boolean: " + boolvalue + "\n");
+            Console.WriteLine("String to boolean: " + boolvalue );
 
-            //int to boolean
             int b = 0;
             bool boolean = Convert.ToBoolean(b);
-            Console.WriteLine("int to boolean: " + boolean + "\n");
+            Console.WriteLine("int to boolean: " + boolean );
 
-            //Converts a type to a byte
             string stri = "10";
             byte bytevalue = Convert.ToByte(stri);
             Console.WriteLine("string to byte: " + bytevalue + "\n");
 
-            //Converts type to a datatime
-            DateTime dt2 = DateTime.Now;
-            Console.WriteLine("update datatime: " + dt2 + "\n");
 
-            //Converts type to a decimal
-            string stringVal = "2,345.26";
-            decimal.Parse(stringVal);
-            Console.WriteLine("convert to decimal:" + stringVal + "\n");
+            //implisity conversion
+            Console.WriteLine("Implisity method: " );
 
-
-            // float to double
             float floatNum = 3.14f;
             double doubleNum = floatNum;
-            Console.WriteLine("convert to double:" + doubleNum + "\n");
+            Console.WriteLine("Float to double: "+doubleNum );
+
+            int num = 100;
+            long bigNum = num;
+            Console.WriteLine("Int to long:"+bigNum);
+
+            char st = 'A';
+            int asciiValue = st;
+            Console.WriteLine("Char to asciivalue: "+asciiValue);
+
+
+            int nume = 10;
+            double decimalNum = nume;
+            Console.WriteLine("Int to double: "+decimalNum);
+
+            byte it = 255; 
+            int its = it;
+            Console.WriteLine("Byte to int:"+its + "\n");
+
+
+            //Explicit Conversion
+            Console.WriteLine("Explicit method: " + "\n");
+
+            double d3 = 9.622345785796;
+            float f3 = (float)d3;
+            Console.WriteLine("Double to float: "+f3);
+
+            double d1 = 5.009754789999;
+            int d2 = (int)d1;
+            Console.WriteLine("Double to int: "+d2);
+
+            float f1 = 4.7680f;
+            int f2 = (int)f1;
+            Console.WriteLine("Float to int: "+f2);
+
+            int b3 = 245;
+            byte b2 = (byte)b3;
+            Console.WriteLine("Int to byte: "+ b2);
+
+            int ch = 97;
+            char ch2 = (char)ch;
+            Console.WriteLine("Int to char: "+ch2+"\n");
+
+
+
+            //parseing conversion
+            Console.WriteLine("parseing method: " + "\n");
 
             string a = "1234";
             int b1 = int.Parse(a);
             Console.WriteLine("string to int: " + b1);
+
+            string s1 = "7.65449554";
+            double de = double.Parse(s1);
+            Console.WriteLine(de);
+
+            string stringVal = "2,345.26";
+            var dd = decimal.Parse(stringVal);
+            Console.WriteLine(dd.GetType());
+            Console.WriteLine(decimal.MaxValue);
+
+            string httpTime = "Fri, 27 Feb 2009 03:11:21 GMT";
+            DateTime time = DateTime.Parse(httpTime);
+            Console.WriteLine(time);
+
+
+            //Try parse method
+            string text1 = "x";
+            int num1;
+            bool res = int.TryParse(text1, out num1);
+            if (res == false)
+            {
+                Console.WriteLine(num1);
+            }
+
+            string text2 = "10000";
+            int num2;
+            if (int.TryParse(text2, out num2))
+            {
+                Console.WriteLine(num2);
+            }
+
+            const string value3 = "345";
+            if (int.TryParse(value3, out int result))
+            {
+                Console.WriteLine(result + 1);
+            }
         }
     }
 }
